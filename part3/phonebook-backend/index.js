5889d3f332f51393ae5f3f7ed97e202bdac6b093
Damilola <damiactuary@gmail.com>
2026-02-26 11:14:50 +0100

    add frontend dist to backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 // Middleware
 // --------------------
 app.use(cors());
+app.use(express.static("dist")); 
 app.use(express.json());
 
 // Custom morgan token to log request body (3.8)
