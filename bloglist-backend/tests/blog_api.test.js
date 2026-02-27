d4b5b0a0b6fa17161f338bb24e87a545e747267d
Damilola <damiactuary@gmail.com>
2026-02-27 11:13:56 +0100

    exercises 4.13-4.14 delete and update blog

--- bloglist-backend/tests/blog_api.test.js
+++ bloglist-backend/tests/blog_api.test.js
@@ -84,6 +84,37 @@ describe("POST /api/blogs", () => {
   });
 });
 
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+    const ids = blogsAtEnd.map((b) => b.id);
+    assert(!ids.includes(blogToDelete.id));
+  });
+});
+
+describe("updating a blog", () => {
+  test("succeeds in updating likes", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
+
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200);
+
+    assert.strictEqual(response.body.likes, blogToUpdate.likes + 1);
+  });
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
